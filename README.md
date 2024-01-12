1cbbe0abc3e6a4eaa862709cf84b53fd9c4bab24
Marius Du Plooy <woestyn101@gmail.com>
2024-01-11 23:27:10 -0500

    questions added

--- README.md
+++ README.md
@@ -1,7 +1,38 @@
-# 1
+# blue
 
-## Description 
-2
-3
-4
-5
+## Description
+
+red
+jack
+forem
+jackson
+
+## Table of Contents:
+
+[Installation](#Installation)
+[Usage](#usage)
+[Credits](#credits)
+[License](#license)
+[Questions](#questions)
+
+## Installation
+
+do it
+
+## Usage
+
+fun
+
+## Credits
+
+youtube
+
+## License
+
+GNU General Public License v3.0
+
+## Questions
+
+woestyn101
+wwww
+22@er.com
--- index.js
+++ index.js
@@ -43,6 +43,53 @@ inquirer
         message: "What did you learn?",
         default: ""
     },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "What are the installation instructions?",
+        default: ""
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "What is the usage of this project?",
+        default: ""
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: "Any credits/contributions to this project?",
+        default: ""
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "Choose a license:",
+        choices: ['Apache License 2.0',
+             'GNU General Public License v3.0 ', 
+            'MIT License',
+            'Mozilla Public License 2.0',
+            'Boost Software License 1.0'],
+        default: "MIT License"
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: "What is you github username?",
+        default: ""
+    },
+    {
+        type: 'input',
+        name: 'githubProfile',
+        message: "What is your github profile link?",
+        default: ""
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?",
+        default: ""
+    },
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
@@ -66,7 +113,25 @@ inquirer
      data.motivation + "\n" +   
      data.build + "\n" + 
      data.solve + "\n" + 
-     data.learn + "\n" , () => {
+     data.learn + "\n\n" +
+     "## Table of Contents: " + "\n\n" +
+     "[Installation](#Installation)" + "\n" + 
+     "[Usage](#usage)" + "\n" + 
+     "[Credits](#credits)" + "\n" + 
+     "[License](#license)" + "\n" + 
+     "[Questions](#questions)" + "\n\n" + 
+     "## Installation"+ "\n\n" + 
+     data.installation + "\n\n" + 
+     "## Usage" + "\n\n" +
+     data.usage + "\n\n" + 
+     "## Credits" + "\n\n" +
+     data.credits + "\n\n" +
+     "## License" + "\n\n" + 
+     data.license + "\n\n" + 
+     "## Questions" + "\n\n" + 
+     data.username + "\n" + 
+     data.githubProfile + "\n" +
+     data.email, () => {
         console.log("file was written");
     } );
   }
