ceb0eb07cb4adeea491df882a1b7a97fd51c40ee
Marius Du Plooy <woestyn101@gmail.com>
2024-01-12 01:11:23 -0500

    top margin added

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ inquirer
   function writeToFile(data){
     fs.writeFile('./README.md', 
     
-    "# " + data.title + "\n\n" +
+    "\n" + "# " + data.title + "\n\n" +
     "## Description " + "\n" +
      data.motivation + "\n" +   
      data.build + "\n" + 
